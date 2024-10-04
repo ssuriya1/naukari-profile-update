bddb4a5a8623c0346a3463a6e18928c8a2c85a15
SURIYA <suriyastar100@gmail.com>
2024-10-04 23:49:18 +0530

    Update code

--- naukari-update.py
+++ naukari-update.py
@@ -11,7 +11,14 @@ import time
 username = os.getenv('NAUKRI_USERNAME')
 password = os.getenv('NAUKRI_PASSWORD')
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--window-size=1920,1080")
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 driver.get("https://www.naukri.com/nlogin/login")
 timeout = 20
 
