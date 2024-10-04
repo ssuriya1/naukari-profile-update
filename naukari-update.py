ea57979199017adaa5c6ea6380f412aefbbb71a7
SURIYA <suriyastar100@gmail.com>
2024-10-04 18:46:56 +0530

    Update naukari-update.py

--- naukari-update.py
+++ naukari-update.py
@@ -2,15 +2,24 @@ import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--window-size=1920,1080")
+chrome_options.add_argument("--disable-extensions")
+
 username = os.getenv('NAUKRI_USERNAME')
 password = os.getenv('NAUKRI_PASSWORD')
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 driver.get("https://www.naukri.com/nlogin/login")
 driver.find_element(By.ID, 'usernameField').send_keys(username)
 driver.find_element(By.ID, 'passwordField').send_keys(password)
@@ -24,9 +33,11 @@ edit_button = WebDriverWait(driver, 10).until(
 )
 edit_button.click()
 time.sleep(3)
+
 save_button = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.ID, 'saveBasicDetailsBtn'))
 )
 save_button.click()
 time.sleep(3)
+
 driver.quit()
