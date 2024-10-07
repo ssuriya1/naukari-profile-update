81075bd209d51142c640739cfa98ac2ee3c6dd18
SURIYA <suriyastar100@gmail.com>
2024-10-07 23:06:53 +0530

    remoed headless

--- naukari-update.py
+++ naukari-update.py
@@ -13,7 +13,7 @@ username = os.getenv('NAUKRI_USERNAME')
 password = os.getenv('NAUKRI_PASSWORD')
 
 chrome_options = Options()
-chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-gpu")
